0e065c3409ff6dfeba2d1311b12ec6ee20c21eba
Chris Gastaldo <chris.gastaldo@gmail.com>
2024-08-11 07:28:53 -0400

    Finished exercises 05-07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if(number1 <  0 || number2 < 0) {
+        return "ERROR";
+    }
+    else if(number1%1 != 0 || number2%1 !=0){
+        return "ERROR"
+    }
+    else if(typeof(number1) != 'number' || typeof(number2) != 'number') {
+        return "ERROR";
+    }
 
+    if (number1 < number2){
+        minNumber= number1;
+        maxNumber= number2;
+    }
+    else{
+        maxNumber= number1;
+        minNumber= number2;
+    }
+    let sumNumbers = minNumber;
+    for (i=minNumber; i<maxNumber; i++){
+        minNumber +=1;
+        sumNumbers += minNumber;
+    }
+    return sumNumbers;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function round(number){
+  return Math.round(number*10)/10;
+}
+
+const convertToCelsius = function(temperature) {
+  return round((temperature -32)*(5/9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return round((9/5)*temperature+32)
 };
 
 // Do not edit below this line
