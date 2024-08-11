c2711e9fbe69090779ccd74e3b4eb9c90ec7e42e
Chris Gastaldo <chris.gastaldo@gmail.com>
2024-08-10 22:27:16 -0400

    Solved 01-04 of the excercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+    reversedString = ''
+    stringLength = userString.length;
+    for (let i=0; i<stringLength; i++){
+        reversedString = reversedString + 
+            userString.slice(stringLength-i-1,stringLength-i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray) {
+    for (i=1; i<arguments.length; i++){
+        while (userArray.includes(arguments[i])){
+            const index = userArray.indexOf(arguments[i]);
+            if (index > -1){
+                userArray.splice(index, 1);
+            }
+        }
+    }
+    return userArray;
 };
 
 // Do not edit below this line
